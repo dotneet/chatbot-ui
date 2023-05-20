33d063045a241175637948aff068be4d8de65201
zhanyulong <zhanyuilong@gmail.com>
2023-05-20 20:36:46 +0800

    feat(next-auth): remove @next-auth/mongodb-adapter

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,10 +4,8 @@ import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 
 import { getUserHashFromMail } from '@/utils/server/auth';
-import clientPromise from '@/utils/server/mongodb';
 
 import { Magic } from '@magic-sdk/admin';
-import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 
 const magic = new Magic(process.env.MAGIC_SECRET_KEY);
 
@@ -30,9 +28,10 @@ const providers = process.env.NEXTAUTH_ENABLED
           // validate magic DID token
           magic.token.validate(didToken);
           // fetch user metadata
-          const metadata = await magic.users.getMetadataByToken(didToken);
+          const { email } = await magic.users.getMetadataByToken(didToken);
+          const id = getUserHashFromMail(email as string);
           // return user info
-          return { ...metadata };
+          return { id, email };
         },
       }),
     ]
@@ -54,7 +53,6 @@ const providers = process.env.NEXTAUTH_ENABLED
 
 export const authOptions: NextAuthOptions = {
   providers: providers,
-  adapter: MongoDBAdapter(clientPromise),
   session: { strategy: 'jwt' },
   pages: {
     signIn: process.env.NEXTAUTH_ENABLED ? '/auth/login' : '/auth/autologin',
