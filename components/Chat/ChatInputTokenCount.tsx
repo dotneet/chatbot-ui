525b0cbd9e5d045d2cfe4892666ad9c38a870825
Jason Cheng <jasoncky96@gmail.com>
2023-08-11 02:25:22 +0800

    Resolve missing conversation selector on new user setup

--- components/Chat/ChatInputTokenCount.tsx
+++ components/Chat/ChatInputTokenCount.tsx
@@ -4,20 +4,18 @@ import { useTranslation } from 'react-i18next';
 import HomeContext from '@/pages/api/home/home.context';
 
 import { trpc } from '@/utils/trpc';
-import { LocalAIModelID } from '@/types/openai';
-import useConversations from '@/hooks/useConversations';
+import { LocalAIModelID, fallbackModelID } from '@/types/openai';
 
 export function ChatInputTokenCount(props: { content: string | undefined }) {
   const { t } = useTranslation('chat');
-  const [_, conversationsAction] = useConversations();
-
   const {
     state: { selectedConversation },
   } = useContext(HomeContext);
 
+  let modelId = !!selectedConversation?.model ? selectedConversation.model.id as LocalAIModelID : fallbackModelID
   const tokenCount = trpc.tokens.count.useQuery({
-    model: selectedConversation?.model.id as LocalAIModelID,
-    prompt: props.content!
+    model: modelId,
+    prompt: !!props.content ? props.content : ""
   })
   const count = tokenCount.data?.tokenCount;
   if (count == null) return null;
