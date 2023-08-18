942b6019c2e3a7f24f1596d4ffda6676cd013027
Jason Cheng <jasoncky96@gmail.com>
2023-08-19 03:26:43 +0800

    Added generate/migrate/push for postgres db management

---
+++ webapp/drizzle.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from "drizzle-kit";
+
+export default {
+  schema: "./db/schema.ts",
+  out: "./migrations-folder",
+  driver: 'pg',
+  dbCredentials: {
+    connectionString: process.env.POSTGRES_URI!,
+  }
+} satisfies Config;
---
+++ webapp/migrate.ts
@@ -0,0 +1,16 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+import postgres from "postgres";
+import { exit } from "process";
+ 
+const sql = postgres(process.env.POSTGRES_URI!, { max: 1 })
+const db = drizzle(sql);
+
+const migrateDB = async () => {
+    console.log("migrating...")
+    await migrate(db, { migrationsFolder: "migrations-folder" });
+    console.log("migrated!")
+    exit()
+}
+
+migrateDB();
